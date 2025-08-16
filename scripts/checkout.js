33e3694f1ac37885d0fc7bb53e236a7b13e50163
Keval <itkeval2006@gmail.com>
2025-08-17 00:25:19 +0530

    made some changes and fixed money.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,8 @@ cart.forEach((cartItem) => {
     });
 
  cartSummaryHTML += `
-<div class="cart-item-container">
+<div class="cart-item-container 
+    js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
         Delivery date: Tuesday, June 21
     </div>
@@ -30,7 +31,7 @@ cart.forEach((cartItem) => {
                 ${matchingProduct.name}
             </div>
             <div class="product-price">
-                $${formatCurrency(matchingProduct.priceCents)}
+                $${(matchingProduct.priceCents / 100).toFixed(2)}
             </div>
             <div class="product-quantity">
                 <span>
@@ -102,5 +103,10 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
+
+        const container = document.querySelector(
+            `.js-cart-item-container-${productId}`
+        );
+        container.remove();
     });
 });
