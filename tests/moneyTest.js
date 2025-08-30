43e85d2f8225238476e58181a9286bc8c4cc21dc
KVS2025 <kevalshah734@gmail.com>
2025-08-30 15:57:54 +0530

    Add jasmine testing frameworks

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -23,5 +23,5 @@ console.log('rounds up to the nearest cent');
 if(formatCurrency(2000.5) === '20.01'){
     console.log('passed');
 } else{
-    console.log('failed')
+    console.log('failed');
 }
