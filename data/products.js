19460f690a93ea5d762b4d22da1809d7825782bd
KVS2025 <kevalshah734@gmail.com>
2025-10-02 16:46:13 +0530

    Convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -89,6 +89,29 @@ object3.method();
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();  //Asynchronous
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+  });  //Makes HTTP Requests
+
+  return promise;
+}
+/*
+loadProductsFetch().then(() => {
+  console.log('next step')
+});
+*/
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
